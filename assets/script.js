e14759cab2362afda63cc8cc9fd13a81d1aaf5af
KevinKiely <Kevinkiely762@gmail.com>
2024-03-21 18:17:57 -0400

    Added functionality to search button, 2 API calls, and creating elements dynamically using javascript to append them to their respective areas

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,8 @@
 var cityName= document.querySelector('#citySearchInput');
 var searchBtn= document.querySelector('#searchBtn');
+var dateOne= document.querySelector('#date-one');
+var infoOne= document.querySelector('#info-one');
+var fourDayForecast= document.querySelector('#four-day-forecast');
 
 const APIkey= "65bffcd5cc2e4aefbd78b08c160f7d07";
 
@@ -21,41 +24,74 @@ function findCity() {
     fetch(coordinatesURL)
     .then(function(response){
         if(response.ok) {
-            console.log(response);
-            response.json().then(function(data){
-                console.log(data[0].name);
-                console.log(data[0].lat);
-                console.log(data[0].lon);
-                var lat=data[0].lat;
-                var lon=data[0].lon;
-                let chosenCity=data[0].name;
-
-                weatherURL=`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIkey}&units=imperial`;
-                
-                fetch(weatherURL).then(function(response){
-                    if (response.ok) {
-                        console.log(response);
-                        response.json().then(function (data){
-                            console.log(data);
-                            var weatherStats=data.list;
-                            console.log(weatherStats);
-
-                        });
-                    }
-                });
-
-
-
-
-
-
-
-
-            });
+            return response.json();
         } 
-        else {
-            alert(`City not Found!`);
-        }
-    });
+    })
+    .then(function(data){
+        var lat = data[0].lat;
+        var lon = data[0].lon;
+        weatherURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIkey}&units=imperial`;
+        return fetch(weatherURL);
+    })
+    .then(function(response) {
+        if(response.ok) {
+            return response.json();
+        } 
+    })
+    .then(function(data) {
+        console.log(data);
+        var chosenCity = data.city.name;
+        var today = data.list[0].dt_txt;
 
+        // This is the forecast information for today
+        dateOne.textContent = `${chosenCity},${today}`;
+
+        var tempOne= document.createElement('li');
+        var windOne= document.createElement('li');
+        var humidityOne= document.createElement('li');
+
+        tempOne.textContent= `Temperature: ${data.list[0].main.temp} degrees `;
+        windOne.textContent= `Windspeed: ${data.list[0].wind.speed} mph`;
+        humidityOne.textContent= `Humidity: ${data.list[0].main.humidity}%`;
+
+        infoOne.append(tempOne);
+        infoOne.append(windOne);
+        infoOne.append(humidityOne);
+
+
+
+        // This is the forecast information for the next 4 days
+        var weatherStats = data.list;
+        console.log(weatherStats);
+
+        for (var i=8; i < 40; i +=8) {
+            //New Card, one per day for the next 4 days
+            var forecastCard= document.createElement('card');
+            forecastCard.classList= 'text-light bg-dark';
+
+            // Adding Date and forecast information, appending to cards
+            var nextDay= document.createElement('h6');
+            nextDay.textContent=weatherStats[i].dt_txt;
+            nextDay.classList= 'text-light bg-dark fs-2';
+            forecastCard.appendChild(nextDay);
+            
+
+            var nextDayTemp= document.createElement('p');
+            nextDayTemp.textContent=weatherStats[i].main.temp;
+            nextDayTemp.classList= 'text-light bg=dark fs=4';
+            forecastCard.appendChild(nextDayTemp);
+
+
+
+            fourDayForecast.appendChild(forecastCard);
+
+
+
+
+
+        }
+
+
+        
+    })
 };
